deada876c08edf76e214e4a7a4dc884e0798891b
Arsany Ashraf <arsanyarsany4800@gmail.com>
2024-09-03 12:45:05 +0300

    add category and subcategory

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
-import express from 'express';
+import express, { Router } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import DBconnection from './config/config';
+import categoriesRoute from './routes/categoriesroute';
+import subCategoryRoute from './routes/subCategoryRoute';
+dotenv.config();
+DBconnection();
 
-const app = express();
-const port = 5000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript ');
-});
+const app:express.Application = express();
+app.use(express.json())
+app.use("/api/Category/",categoriesRoute);
+app.use("/api/subCategory",subCategoryRoute)
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
